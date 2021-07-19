4ee020eb14fa63e03e97b517faf40d5afb794395
Shreyasreddy863 <78153431+Shreyasreddy863@users.noreply.github.com>
2021-07-19 16:23:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+function setup(){
+    canvas = createCanavas(300,300);
+    canvas.center();
+    video = createCapture(video);
+    video.hide();
+classifier = ml5.imageClassfier('https://teachblemachine.withgoogle.com/models/v_sl95BzE/model.json',modelloaded);
+}
+
+function draw(){
+    image(video,0,0,300,300);
+    classifier.classify(video, gotResult);
+
+}
+
+function modelloaded(){
+    console.log('Model Loaded');
+}
+
+function gotResult(error, results) {
+    if (error ) {
+        console.error(error);
+    } else {
+console.log(results);
+document.getElementById("result_object_name").innerHTML = results[0].label;
+document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
+}
